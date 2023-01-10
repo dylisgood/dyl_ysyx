f73ba533ac3091d590b309a5bb66eac3947768aa
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-10 19:30:44 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:30:44 up  9:10,  1 user,  load average: 0.18, 0.07, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -43,13 +43,13 @@ static char* rl_gets() {
 }
 
 static int cmd_c(char *args) {
-  cpu_exec(-1);
+  cpu_exec(1);
   return 0;
 }
 
 
 static int cmd_q(char *args) {
-//  cpu_exec(-1);
+  cpu_exec(-1);
   return -1;
 }
 
