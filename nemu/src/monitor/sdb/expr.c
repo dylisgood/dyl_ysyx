82f60c583dd2a14cecbc4afa28150187ae51206b
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-14 16:33:35 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:35 up  7:43,  1 user,  load average: 0.09, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -108,10 +108,12 @@ static bool make_token(char *e) {
            {
              strncpy(tokens[j-1].str,substr_num,NUM_number);
              tokens[j-1].type = rules[i-1].token_type;
+             printf("j = %d,  str= %s \n", j-1, tokens[j-1].str); 
            }   
            NUM_FLAG = 0;
            NUM_number  = 0;
            tokens[j].type = rules[i].token_type;
+           printf("j = %d,  str= %s \n", j, tokens[j].str); 
            j++;
         }
         else if(rules[i].token_type == NUM)
@@ -120,7 +122,6 @@ static bool make_token(char *e) {
             NUM_number ++;
             NUM_FLAG = 1;
         }
-        printf("j = %d,  str= %s \n", j, tokens[j].str); 
 //        switch (rules[i].token_type) {
 //          default: TODO();
 //      }
