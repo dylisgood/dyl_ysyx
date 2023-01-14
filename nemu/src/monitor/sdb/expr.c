f1479be32170ae8575147710443529e5306c43fc
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-14 21:06:18 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:06:18 up 12:16,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -117,10 +117,10 @@ static bool make_token(char *e) {
               NUM_FLAG = 0;
               NUM_number =0;
            } 
-           else {  
+            
            tokens[j].type = rules[i].token_type;
            if(rules[i].token_type != TK_NOTYPE) { j++; }
-           }
+           
         }
         else if(rules[i].token_type == NUM && e[position + 1] != '\0')
         { 
