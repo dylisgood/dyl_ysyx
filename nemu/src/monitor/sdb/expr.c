db28ef809b81d617820029ff1d6be946fe4ab409
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 16:52:33 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:33 up  8:02,  1 user,  load average: 0.13, 0.31, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -241,6 +241,7 @@ uint32_t eval(int p,int q){
       op = Main_position(p,q);
       val1 = eval(p, op - 1);
       val2 = eval(op+1, q);
+      printf("val1 = %d,  val2 = %d\n", val1,val2);
       op_type = tokens[op].type;
       switch(op_type){
         case '+':return val1 + val2;
