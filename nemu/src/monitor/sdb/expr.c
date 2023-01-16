63b0d9430a52b33298dc717a9b4187bac1d68aa9
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-16 22:10:43 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:43 up 13:21,  1 user,  load average: 0.11, 0.05, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -244,7 +244,7 @@ uint32_t eval(int p,int q){
     uint32_t val1,val2;
     char op_type;
     int op;
-    printf("p=%d,   q=%d\n",p,q);
+//    printf("p=%d,   q=%d\n",p,q);
     if(p > q){
       printf("bad expression! \n");
       assert(0);
@@ -286,7 +286,7 @@ word_t expr(char *e, bool *success) {
   uint32_t result=0;
   if(check_expr(0,numofstr)){
      result = eval(0,numofstr);
-     printf("result = %d\n ",result);
+//     printf("result = %d\n ",result);
   }
   else printf("the expr is false\n");
   
