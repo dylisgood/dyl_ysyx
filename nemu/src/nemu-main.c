1ad165b13cf1c4edd2b90c9bd3da82c429a03278
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-18 21:54:41 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:41 up 12:24,  1 user,  load average: 0.83, 0.36, 0.18

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -28,12 +28,12 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-   //char buf[1000];
-   //int count;
-   //char buff[1000];
+   char buf[1000];
+   int count;
+   char buff[1000];
    uint32_t result=0;
    bool* success = false;
-   /*FILE *fp = fopen("/home/ysyx/ysyx-workbench/nemu/tools/gen-expr/input","r");
+   FILE *fp = fopen("/home/ysyx/ysyx-workbench/nemu/tools/gen-expr/input","r");
    
    for(int i=0; i < 100; i++){
    count = fscanf(fp,"%s",buff);
@@ -53,11 +53,13 @@ int main(int argc, char *argv[]) {
   //printf("the result = %ld, count = %d\n",result,count);
    //printf("the count = %s,the buff is%s",buf,buff);
    }
-   */
+   
+   /*
    for(int i=0;i<100;i++){
    result = expr("1*(10*(1)-8-(((((((10))))))))",success);
    printf("result = %d\n",result);
    }
+   */
   /* Start engine. */
   engine_start();
 
