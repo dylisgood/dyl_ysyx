dc6afb58694209636a0a319566af245b73db987e
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 22:33:30 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:33:30 up 13:24,  1 user,  load average: 0.08, 0.26, 0.27

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -150,7 +150,7 @@ static bool make_token(char *e) {
                       if(NUM_FLAG){j++; NUM_FLAG = 0;HEX_NUM_FLAG =0;} 
                       printf("i enter case\n");
                       tokens[j].type = rules[i].token_type; 
-                      j++; 
+                      if(e[position] != '\0') {j++;} 
                       break;
           case NUM:
                       printf("I enter NUM\n");
