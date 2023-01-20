1247512fe4034eb3f147dff17025924d49cb2de1
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-20 21:10:00 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:10:00 up 12:00,  1 user,  load average: 0.32, 0.18, 0.17

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -159,13 +159,13 @@ static bool make_token(char *e) {
       }
 
       } 
-    
+    } 
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
 
-    } 
+     
   //} 
   }
   numofstr = j - 1;
