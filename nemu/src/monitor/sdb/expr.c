5e30bcaaefd978099553f0e6d8609439bddb9088
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 21:46:43 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:43 up 13:00,  1 user,  load average: 0.37, 0.18, 0.12

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -267,8 +267,11 @@ uint64_t eval(int p,int q){
       else if(tokens[p].type == TK_REG)
       {
         return isa_reg_str2val("t0",succ);
-       // return 10;
       }        
+      else if(tokens[p].type == HEX_NUM)
+      {
+        return 100;
+      }
       else 
       {return atoi(tokens[p].str);}
     }
