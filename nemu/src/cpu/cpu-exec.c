0886af98d5f4eb2003e78a89075a0d62008ddaa4
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-06 10:20:40 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux RISCV 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:40 up  1:34,  1 user,  load average: 0.20, 0.18, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,6 +42,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+  if(CONFIG_WATCHPOINT) {printf("defined watchpoint! \n");}
+
   WP  *pb;
   bool *suc = false;
   pb = head;
