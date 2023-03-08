1df5afcfddacd8a0c756bae8f269caa2c8a6b188
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 10:43:02 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux DYL 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:02 up 22:46,  2 users,  load average: 1.88, 1.87, 1.46

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -29,7 +29,7 @@ override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
-NEMU_EXEC := $(BINARY) -b $(ARGS) $(IMG)
+NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
