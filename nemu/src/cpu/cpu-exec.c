13add4da4b303e10f0b5c4f1e386df5c4201607a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 19:15:42 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux DYL 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:15:42 up 3 days,  7:32,  4 users,  load average: 0.51, 0.60, 0.72

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -159,7 +159,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   else if((s->isa.inst.val & 0x67) == 0x67){
     printf("find jalr!   ");
     printf("s->pc = %lx ",s->pc);
-    printf("s->pc = %lx\n",s->dnpc);
+    printf("s->pc = %lx ",s->dnpc);
     for (int i = 0; i < jj; i++) {
         Elf64_Sym *sym = &symbols[i];
         if(sym->st_info == 18){
