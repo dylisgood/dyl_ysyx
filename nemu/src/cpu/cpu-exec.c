b8a5bbb7bc955873c062aaaa922aa43bfbc652af
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-15 19:28:58 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux DYL 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:28:58 up 3 days,  7:45,  4 users,  load average: 0.64, 0.31, 0.42

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -242,13 +242,13 @@ void cpu_exec(uint64_t n) {
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
-    case NEMU_END: case NEMU_ABORT:{
+    case NEMU_END: case NEMU_ABORT:
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? (print_iringbuf(), ANSI_FMT("ABORT", ANSI_FG_RED)) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-      close_ftrace();}
+    
       // fall through
     case NEMU_QUIT: {statistic();close_ftrace();}
   }
