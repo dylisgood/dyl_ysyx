7125856c7ee42198d3f9f0b69dcfa17ab65ffcbf
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 21:45:18 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux DYL 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:18 up 1 day, 12:55,  4 users,  load average: 0.14, 0.39, 0.48

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -55,7 +55,7 @@ char *strncat(char *dst, const char *src, size_t n){
 
 //  ‘\0’ = 0
 int strcmp(const char *s1, const char *s2) {
-  while(*s1 && *s1 == *s2){
+  while(*s1 && (*s1 == *s2)){
       s1++;
       s2++;
   }
