75936b0884f1c7c75dd32d2b54b4d6d2750885d1
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-17 21:59:05 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux DYL 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:05 up 1 day, 13:08,  4 users,  load average: 0.71, 0.83, 0.80

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -182,5 +182,5 @@ void cpu_exec(uint64_t n) {
       // fall through
     case NEMU_QUIT: {statistic();}
   }
-  if(nemu_state.state == NEMU_ABORT)print_iringbuf();
+  //if(nemu_state.state == NEMU_ABORT)print_iringbuf();
 }
