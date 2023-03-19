a592e15c4bedd2777bb88de7e2b4cb1d1c5c8677
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-19 12:14:24 +0800

    > compile NEMU ysyx_22050854 段云栊 Linux DYL 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:14:24 up 1 day,  2:13,  2 users,  load average: 0.54, 0.19, 0.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -26,8 +26,9 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT 21
-//#define CONFIG_FTRACE 0
+
 char iringbuf[10][128] = {};
+
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
